23b00cae45db887a1cfde62bbd1e61f9b4f58dff
Bruno Alejandro Gorosito <125034173+bruno-gorosito@users.noreply.github.com>
2024-11-18 12:04:06 -0300

    a

---
+++ database/migrations/2024_11_18_145550_create_stock_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('cantidad');
+            $table->unsignedBigInteger('producto_id');
+            $table->foreign('producto_id')->references('id')->on('producto');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock');
+    }
+};
