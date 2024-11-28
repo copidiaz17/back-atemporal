9dee20c9d8a77a939e5e456c6997527dfa968884
Bruno Alejandro Gorosito <125034173+bruno-gorosito@users.noreply.github.com>
2024-11-28 20:13:02 -0300

    quitando storage

--- app/Filament/Resources/ProductoResource.php
+++ app/Filament/Resources/ProductoResource.php
@@ -22,7 +22,7 @@ use PhpParser\Node\Stmt\Label;
 class ProductoResource extends Resource
 {
     protected static ?string $model = Producto::class;
-    
+
     protected static ?string $navigationGroup = 'Productos';
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
@@ -44,7 +44,7 @@ class ProductoResource extends Resource
                     ->label('Imagen del Producto')
                     ->required()
                     ->image()
-                    ->directory('storage/images/productos')
+                    ->directory('images/productos')
                     ->disk('public'),
                 Forms\Components\TextInput::make('producto_precio')
                     ->required()
@@ -56,7 +56,7 @@ class ProductoResource extends Resource
                     ->options(Categoria::all()->pluck('categoria_nombre', 'id'))
                     // ->relationship('categoria', 'categoria_nombre')
                     ->searchable(),
-                    Forms\Components\TextInput::make('producto_cantidad')
+                Forms\Components\TextInput::make('producto_cantidad')
                     ->required()
                     ->integer()
                     ->label('Cantidad'),
@@ -71,28 +71,28 @@ class ProductoResource extends Resource
                     ->searchable()
                     ->label('Nombre')
                     ->sortable(),
-                    Tables\Columns\TextColumn::make('stock.cantidad')
-                ->label('Cantidad')
-                ->sortable()
-                ->default(0)
-                ->getStateUsing(fn ($record) => $record->stock?->cantidad ?? 0),
+                Tables\Columns\TextColumn::make('stock.cantidad')
+                    ->label('Cantidad')
+                    ->sortable()
+                    ->default(0)
+                    ->getStateUsing(fn($record) => $record->stock?->cantidad ?? 0),
                 //Tables\Columns\TextColumn::make('producto_descripcion')
-                  //  ->searchable()
-                    //->label('Descripcion'),
+                //  ->searchable()
+                //->label('Descripcion'),
                 Tables\Columns\ImageColumn::make('producto_imagen')
                     ->label('Imagen')
                     ->circular(),
-                    //->disk('public')
-                    //->url(fn ($record) => $record->producto_imagen ? url('storage/images/productos/' . $record->producto_imagen) : url('images/productos/default-image.jpg')),
-                    Tables\Columns\TextColumn::make('producto_cantidad')
+                //->disk('public')
+                //->url(fn ($record) => $record->producto_imagen ? url('storage/images/productos/' . $record->producto_imagen) : url('images/productos/default-image.jpg')),
+                Tables\Columns\TextColumn::make('producto_cantidad')
                     ->label('Cantidad')
-    ->sortable(),
-                
-                    Tables\Columns\TextColumn::make('producto_precio')
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('producto_precio')
                     ->numeric()
                     ->sortable()
                     ->label('Precio'),
-                    Tables\Columns\TextColumn::make('categoria.categoria_nombre')
+                Tables\Columns\TextColumn::make('categoria.categoria_nombre')
                     ->label('Categoría')
                     ->sortable()
             ])
